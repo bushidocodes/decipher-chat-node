d71117da49a9cb5a4fb8c7b93351cfc2ee747a12
Sean McBride <spmcbride1201@gmail.com>
2017-10-20 12:55:14 -0400

    feat: Replay old messages when user joins

--- index.js
+++ index.js
@@ -11,12 +11,26 @@ app.get("/client.js", function(req, res) {
   res.sendFile(__dirname + "/client.js");
 });
 
+// In-memory data structure persisting all messages
+const allTheMessages = [];
+
 // io.emit broadcasts to all including sender
 io.on("connection", function(socket) {
+  console.log(`${socket.id} has joined the chat`);
+  // When a new socket connection is established,
+  // replay all of the past messages
+  allTheMessages.forEach((msg, idx) => {
+    console.log(`Attempting to send message #${idx} to ${socket.id}`);
+    socket.emit("chat message", msg);
+  });
+  // Something here sending past messages to a new user
   socket.on("chat message", function(msg) {
     // Add timestamp
     msg.timestamp = JSON.stringify(Date.now());
-    console.log("What does this message look like: ", msg);
+    console.log("New Message: ", msg);
+    // Save to in memory data structure
+    allTheMessages.push(msg);
+    console.log("In-memory store of messages: ", allTheMessages);
     io.emit("chat message", msg);
   });
 });
